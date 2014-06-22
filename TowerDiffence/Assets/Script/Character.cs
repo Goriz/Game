9161edef41e9bb6649c3b44d5458ff53017fd54d
Yuhei Ichikawa <iyuhei1900@gmail.com>
2014-06-22 15:22:02 +0900

    Create Tower Diffence Game

---
+++ TowerDiffence/Assets/Script/Character.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+	
+	// アニメーターコンポーネント
+	private Animator animator;
+	private int doWalkLeftId, doWalkRightId;
+	private GotoMouse gotomouse;
+
+	public int attack = 1;
+
+	// Use this for initialization
+	void Start () {
+		// アニメーターコンポーネントを取得
+		animator = GetComponent<Animator> ();
+		doWalkLeftId = Animator.StringToHash ("DoWalkLeft");
+		doWalkRightId = Animator.StringToHash ("DoWalkRight");
+
+		gotomouse = GetComponent<GotoMouse> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			if (Input.GetMouseButton(0)) {
+				animator.SetBool(doWalkLeftId, true);
+			}else{
+				animator.SetBool(doWalkLeftId, false);
+			}
+	}
+	
+	// ぶつかった瞬間に呼び出される
+	void OnTriggerStay2D (Collider2D c)
+	{
+			animator.SetTrigger ("Attack");
+			
+	}
+}
---
+++ TowerDiffence/Assets/Script/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public int hp = 100;
+	
+	// アニメーターコンポーネント
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		// アニメーターコンポーネントを取得
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// ぶつかった瞬間に呼び出される
+	void OnTriggerStay2D (Collider2D c)
+	{
+		animator.SetTrigger ("Damage");	
+		
+		// ヒットポイントを減らす
+		hp = hp - 1;
+
+
+		// ヒットポイントが0以下であれば
+		if(hp <= 0 )
+		{
+			// エネミーの削除
+			Destroy (gameObject);
+		}
+	}
+}
