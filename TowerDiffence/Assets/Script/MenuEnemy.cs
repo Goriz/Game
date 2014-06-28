90b32e7790e542ea3a6396e8cf5d59bd785695aa
Yuhei Ichikawa <iyuhei1900@gmail.com>
2014-06-28 21:04:50 +0900

    メニュー画面のアニメーションを追加

---
+++ TowerDiffence/Assets/Script/MenuEnemy.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuEnemy : MonoBehaviour {
+	public int speed = 1;
+	public bool chase = true;
+	private GameObject player = null;
+
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find("MenuPlayer");
+
+		// lifeTime秒後に削除
+		Destroy (gameObject, 9);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (chase == true) {
+			transform.position = Vector2.MoveTowards (transform.position, new Vector2 (-8, 0) , speed * Time.deltaTime);
+		}
+		if (chase == false) {
+			transform.position = Vector2.MoveTowards (transform.position, new Vector2 (6, 0), speed * Time.deltaTime);
+		}
+	}
+}
---
+++ TowerDiffence/Assets/Script/MenuPlayer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuPlayer : MonoBehaviour {
+	public int speed = 1;
+	public bool chase = true;
+	private GameObject enemy = null;
+
+	// Use this for initialization
+	void Start () {
+		enemy = GameObject.Find("MenuEnemy");
+
+		Destroy (gameObject, 8);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (chase == true) {
+		transform.position = Vector2.MoveTowards (transform.position, enemy.transform.position , speed * Time.deltaTime);
+				}
+		if (chase == false) {
+			transform.position = Vector2.MoveTowards (transform.position, new Vector2 (-6, 0), speed * Time.deltaTime);
+				}
+	}
+}
