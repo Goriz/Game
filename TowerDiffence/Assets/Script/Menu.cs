6cc105723c29fc8768cf01e7bc3a7956283ae63a
Yuhei Ichikawa <iyuhei1900@gmail.com>
2014-06-28 19:09:51 +0900

    Switched scene

---
+++ TowerDiffence/Assets/Script/Menu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+	public GameObject chase;
+	public GameObject chased;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Application.LoadLevel ("TowerDiffence");
+		}
+
+	}
+}
