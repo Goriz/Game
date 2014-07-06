f16387e5854b1e170e8fcb341c24ad14415ff427
Kodai <blacktribeard.gamestudio@gmail.com>
2014-07-06 22:16:04 +0900

    あ

---
+++ InfinityDungeon/Assets/Scripts/All/ParcticalSystem1.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParcticalSystem1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		particleSystem.renderer.sortingLayerName = "Background";
+		particleSystem.renderer.sortingOrder = 5;
+	}
+}
