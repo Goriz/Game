6fb0130c3956f8250c162c0d0ebae78e0c8f88f4
Kodai <blacktribeard.gamestudio@gmail.com>
2014-07-06 18:10:21 +0900

    scene trangition added

---
+++ InfinityDungeon/Assets/Scripts/Menu/GameStart.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStart : MonoBehaviour {
+
+	private FadeManager manager;
+
+	private bool isClick = false;
+
+	// Use this for initialization
+	void Start () {
+		manager = FindObjectOfType<FadeManager>();
+	}
+	
+	void OnClick(){
+		if(!isClick){
+			isClick = true;
+			manager.LoadLevel("Menu",1.0f);
+		}
+	}
+}
