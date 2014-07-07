e740504db815b7e8f1139b0941e3d3c2acb0256b
Kodai <blacktribeard.gamestudio@gmail.com>
2014-07-08 00:23:40 +0900

    スクロールバーの検討中

--- InfinityDungeon/Assets/Scripts/Menu/GameStart.cs
+++ InfinityDungeon/Assets/Scripts/Menu/GameStart.cs
@@ -15,7 +15,7 @@ public class GameStart : MonoBehaviour {
 	void OnClick(){
 		if(!isClick){
 			isClick = true;
-			manager.LoadLevel("MainGame",1.0f);
+			manager.LoadLevel("Prepare",1.0f);
 		}
 	}
 }
