a25582fa92c009afbdbb0df679eb9c5611f3be44
Miyady03 <wi11fu1.03j@gmail.com>
2014-07-16 16:25:01 +0900

    複数のエッグマンを表示

--- EggMan/Assets/Scripts/EggMan.cs
+++ EggMan/Assets/Scripts/EggMan.cs
@@ -5,32 +5,38 @@ public class EggMan : MonoBehaviour {
 
 
 	public GameObject eggman;
-
 	public GameObject Break;
+	public int Point;
+
+	//private int i=1;
 
 	// Update is called once per frame
 	void Update () {
+		//Touch tch = Input.GetTouch(0);
+		//Debug.Log (tch);
 		//if (Input.GetMouseButtonDown(0)){
-		if (Input.touchCount > 0){
+		if (Input.touchCount > 0 ){
+			Touch tch = Input.GetTouch(0);
+			if(tch.phase == TouchPhase.Began){
 
-			//Vector3 v = Input.mousePosition;
-			//Vector2 egg = eggman.transform.position;
-			//v.z = 10;
-		    //v = Camera.main.ScreenToWorldPoint(v);
+				//Vector3 v = Input.mousePosition;
+				//Vector2 egg = eggman.transform.position;
+				//v.z = 10;
+			    //v = Camera.main.ScreenToWorldPoint(v);
+				//Touch tch = Input.GetTouch(0);
+				Vector3 v = tch.position;
+				Vector2 egg = eggman.transform.position;
+				v.z = 10;
+				v = Camera.main.ScreenToWorldPoint(v);
 
-			Vector3 v = Input.GetTouch(0).position;
-			Vector2 egg = eggman.transform.position;
-			v.z = 10;
-			v = Camera.main.ScreenToWorldPoint(v);
-
-			//Debug.Log(v);
-			//Debug.Log(Input.mousePosition);
-			//Debug.Log(eggman.transform.localPosition);
-			if( v.x >= egg.x-0.5 && v.x <=egg.x+0.5 && v.y >= egg.y-0.5 && v.y <= egg.y+0.5){
-				Destroy (eggman);
-				Instantiate (Break, eggman.transform.position, eggman.transform.rotation);
+				//Debug.Log(v);
+				//Debug.Log(Input.mousePosition);
+				//Debug.Log(eggman.transform.localPosition);
+				if( v.x >= egg.x-0.8 && v.x <=egg.x+0.8 && v.y >= egg.y-0.8 && v.y <= egg.y+0.8){
+					Destroy (eggman);
+					Instantiate (Break, eggman.transform.position, eggman.transform.rotation);
+				}
 			}
-
 		}
 	}
 }
--- EggMan/Assets/Scripts/MakeEggMan.cs
+++ EggMan/Assets/Scripts/MakeEggMan.cs
@@ -5,14 +5,46 @@ public class MakeEggMan : MonoBehaviour {
 
 
 	public GameObject eggman;
+	public GameObject eggman2;
+	public GameObject eggman3;
+	public GameObject eggman4;
+	public GameObject eggman5;
+	public GameObject eggman6;
+	public GameObject eggman7;
+	public GameObject eggman8;
 	// Use this for initialization
 	IEnumerator Start ()
-	{
+	{	
+		GameObject test = null;
+		int i;
 		while (true) {
 
 			//float rnd_chara = Random.value * 8;
 			//Debug.Log(rnd_chara);
 			//Debug.Log(Mathf.FloorToInt(rnd_chara));
+			//GameObject eggman = GameObject.Find("EggMan");
+			float rndEgg = Random.value * 8;
+			i = Mathf.FloorToInt(rndEgg);
+			//test = eggman;
+			switch(i){
+				case 0 : test = eggman;
+					break;
+				case 1 : test = eggman2;
+					break;
+				case 2 : test = eggman3;
+					break;
+				case 3 : test = eggman4;
+					break;
+				case 4 : test = eggman5;
+					break;
+				case 5 : test = eggman6;
+					break;
+				case 6 : test = eggman7;
+					break;
+				case 7 : test = eggman8;
+					break;
+			}
+				
 
 			float place_vector = Random.value * 3;
 			float place_direction = Random.value;
@@ -22,9 +54,11 @@ public class MakeEggMan : MonoBehaviour {
 			Vector2 appearPoint = new Vector2(place_vector,6);
 
 			// 弾をプレイヤーと同じ位置/角度で作成
-			Instantiate (eggman, appearPoint, transform.rotation);
+			Instantiate (test, appearPoint, transform.rotation);
+			//Instantiate (test, appearPoint, transform.rotation);
 			// 0.05秒待つ
 			yield return new WaitForSeconds (0.5f);
+			Debug.Log(i);
 		}
 	}
 	// Update is called once per frame
